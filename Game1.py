2a6c772f3b5979de935205143c4b2898b906f55e
MarginalpersonQQ <151029444+MarginalpersonQQ@users.noreply.github.com>
2025-12-26 17:43:26 +0800

    fix:Call extenal api issue
    Somebody send API value not  send API function.

--- Game1.py
+++ Game1.py
@@ -9,24 +9,24 @@ class Game1Scene:
         self.font = font
         self.gesture = gesture
         self.last_snapshot = {
-            "left_circle": gesture["left_circle"],
-            "right_circle": gesture["right_circle"],
-            "left_horizontal_loop": gesture["left_horizontal_loop"],
-            "right_horizontal_loop": gesture["right_horizontal_loop"],
-            "left_vertical_loop": gesture["left_vertical_loop"],
-            "right_vertical_loop": gesture["right_vertical_loop"],
+            "left_circle": gesture()["left_circle"],
+            "right_circle": gesture()["right_circle"],
+            "left_horizontal_loop": gesture()["left_horizontal_loop"],
+            "right_horizontal_loop": gesture()["right_horizontal_loop"],
+            "left_vertical_loop": gesture()["left_vertical_loop"],
+            "right_vertical_loop": gesture()["right_vertical_loop"],
         }
     def handle_event(self, event):
         pass
 
     def update(self):
         self.last_snapshot = {
-            "left_circle": self.gesture["left_circle"],
-            "right_circle": self.gesture["right_circle"],
-            "left_horizontal_loop": self.gesture["left_horizontal_loop"],
-            "right_horizontal_loop": self.gesture["right_horizontal_loop"],
-            "left_vertical_loop": self.gesture["left_vertical_loop"],
-            "right_vertical_loop": self.gesture["right_vertical_loop"],
+            "left_circle": self.gesture()["left_circle"],
+            "right_circle": self.gesture()["right_circle"],
+            "left_horizontal_loop": self.gesture()["left_horizontal_loop"],
+            "right_horizontal_loop": self.gesture()["right_horizontal_loop"],
+            "left_vertical_loop": self.gesture()["left_vertical_loop"],
+            "right_vertical_loop": self.gesture()["right_vertical_loop"],
         }
         print(self.last_snapshot)
         pass
