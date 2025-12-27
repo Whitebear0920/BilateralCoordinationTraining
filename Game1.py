8985f89a89d63621c02c3022a7d9535d1bc4a7ac
Whitebear0920 <whiteb0920@gmail.com>
2025-12-28 04:26:48 +0800

    feat:add camera

--- Game1.py
+++ Game1.py
@@ -1,4 +1,6 @@
 import pygame
+import numpy as np
+
 import Config
 from ui.Button import Button
 
@@ -8,28 +10,37 @@ class Game1Scene:
         self.next_scene = None
         self.font = font
         self.gesture = gesture
-        self.last_snapshot = {
-            "left_circle": gesture()["left_circle"],
-            "right_circle": gesture()["right_circle"],
-            "left_horizontal_loop": gesture()["left_horizontal_loop"],
-            "right_horizontal_loop": gesture()["right_horizontal_loop"],
-            "left_vertical_loop": gesture()["left_vertical_loop"],
-            "right_vertical_loop": gesture()["right_vertical_loop"],
-        }
+        self.frame_rect = pygame.Rect(0, 0, 320, 240)
     def handle_event(self, event):
         pass
 
     def update(self):
-        self.last_snapshot = {
-            "left_circle": self.gesture()["left_circle"],
-            "right_circle": self.gesture()["right_circle"],
-            "left_horizontal_loop": self.gesture()["left_horizontal_loop"],
-            "right_horizontal_loop": self.gesture()["right_horizontal_loop"],
-            "left_vertical_loop": self.gesture()["left_vertical_loop"],
-            "right_vertical_loop": self.gesture()["right_vertical_loop"],
-        }
-        print(self.last_snapshot)
+        self.update_data()
         pass
 
     def draw(self):
         self.screen.fill((40, 40, 40))
+        self.draw_camera()
+    
+    def update_data(self):
+        temp = self.gesture()
+        self.last_snapshot = {
+            "now_frame": temp["now_frame"],
+            "left_ccw_circle": temp["left_ccw_circle"],
+            "left_cw_circle": temp["left_cw_circle"],
+            "right_ccw_circle": temp["right_ccw_circle"],
+            "right_cw_circle": temp["right_cw_circle"],
+            "left_horizontal_loop": temp["left_horizontal_loop"],
+            "right_horizontal_loop": temp["right_horizontal_loop"],
+            "left_vertical_loop": temp["left_vertical_loop"],
+            "right_vertical_loop": temp["right_vertical_loop"],
+        }
+        print(self.last_snapshot)
+        
+    def draw_camera(self):
+        if self.last_snapshot["now_frame"] is not None:
+            frame = self.last_snapshot["now_frame"]
+            frame = frame[:, :, ::-1]
+            surface = pygame.surfarray.make_surface(frame.swapaxes(0, 1))
+            surface = pygame.transform.scale(surface,(self.frame_rect.width, self.frame_rect.height))
+            self.screen.blit(surface, self.frame_rect)
