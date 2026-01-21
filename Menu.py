e819212b5d236ca4232f033d373ee9dc6dc6c126
Whitebear0920 <whiteb0920@gmail.com>
2026-01-21 19:25:17 +0800

    fix: remove game 2 button

--- Menu.py
+++ Menu.py
@@ -9,20 +9,20 @@ class MenuScene:
         self.next_scene = None
         self.button_font = AssetsManager.get_font("main")
         self.btn_a = Button("遊戲 1", 0, 0, 200, 60, self.button_font)
-        self.btn_b = Button("遊戲 2", 0, 0, 200, 60, self.button_font)
-        self.btn_c = Button("離開", 0, 0, 200, 60, self.button_font)
+        self.btn_b = Button("離開", 0, 0, 200, 60, self.button_font)
+        #self.btn_c = Button("離開", 0, 0, 200, 60, self.button_font)
         self.btn_a.rect.center = (Config.WIDTH//2, Config.HEIGHT//2 - 100)
         self.btn_b.rect.center = (Config.WIDTH//2, Config.HEIGHT//2 + 100)
-        self.btn_c.rect.center = (Config.WIDTH//2, Config.HEIGHT//2 + 300)
+        #self.btn_c.rect.center = (Config.WIDTH//2, Config.HEIGHT//2 + 300)
 
     def handle_event(self, event):
         if event.type == pygame.MOUSEBUTTONDOWN:
             if self.btn_a.is_clicked(event):
                 self.next_scene = {"name":"Game1"}
             elif self.btn_b.is_clicked(event):
-                self.next_scene = {"name":"Game2"}
-            elif self.btn_c.is_clicked(event):
                 self.next_scene = {"name":"Exit"}
+            #elif self.btn_c.is_clicked(event):
+            #    self.next_scene = {"name":"Exit"}
 
     def update(self):
         pass
@@ -31,4 +31,4 @@ class MenuScene:
         self.screen.fill((40, 40, 40))
         self.btn_a.draw(self.screen)
         self.btn_b.draw(self.screen)
-        self.btn_c.draw(self.screen)
+        #self.btn_c.draw(self.screen)
