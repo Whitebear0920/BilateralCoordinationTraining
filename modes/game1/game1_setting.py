2a672a8d4f3da652b560c93a83dd0f35562fe7d5
Whitebear0920 <whiteb0920@gmail.com>
2026-03-09 17:24:13 +0800

    feat: add all_select and clear btn

--- modes/game1/game1_setting.py
+++ modes/game1/game1_setting.py
@@ -11,29 +11,41 @@ class Game1Setting:
         
         self.font = AssetsManager.get_font("main")
         
-        self.enabled_action_indices = [Game1Action.VV,Game1Action.HH]
+        self.enabled_action = [Game1Action.VV,Game1Action.HH]
+        self.enabled_action_indices = []
         self.train_duration = 20
         self.action_duration = 30
         self.break_duration = 20
         
         self.action_name = ["雙手水平","雙手垂直","左垂直 + 右水平","左水平 + 右垂直","左逆 + 右順","左逆 + 右逆","左順 + 右逆","左順 + 右順","左垂直 + 右順","左垂直 + 右逆","左逆 + 右垂直","左順 + 右垂直","左水平 + 右逆","左順 + 右水平","左水平 + 右順","左逆 + 右水平",]
         self.action_btn = []
-        start_x = 200
-        start_y = 120
-        gap_y = 60
-        gap_x = 350
+        action_btn_start_x = 200
+        action_btn_start_y = 120
+        action_btn_gap_y = 60
+        action_btn_gap_x = 350
         for i in range(16):
             col = i // 8
             row = i % 8
-            x = start_x + col * gap_x
-            y = start_y + row * gap_y
+            x = action_btn_start_x + col * action_btn_gap_x
+            y = action_btn_start_y + row * action_btn_gap_y
 
             btn = Button(self.action_name[i],x,y,300,50,self.font,config.BLUE)
             self.action_btn.append(btn)
-            pass
-        #self.action_btn = Button("", config.WIDTH//2,50, 300, 50, self.font, config.BLUE)
+        self.all_action_select_btn = Button("全選",900,180,150,100,self.font,config.BLUE)   
+        self.all_action_clear_btn = Button("清除",900,300,150,100,self.font,config.BLUE)   
 
     def handle_event(self, event):
+        for i, btn in enumerate(self.action_btn):
+            if btn.is_clicked(event):
+                action = Game1Action(i)
+                if action in self.enabled_action:
+                    self.enabled_action.remove(action)
+                else:
+                    self.enabled_action.append(action)
+        if self.all_action_select_btn.is_clicked(event):
+            self.enabled_action = list(Game1Action)
+        if self.all_action_clear_btn.is_clicked(event):
+            self.enabled_action.clear()
         pass
     
     def update(self):
@@ -42,11 +54,25 @@ class Game1Setting:
     def draw(self):
         self.screen.fill((40, 40, 40))
         self.draw_text("設置",config.WIDTH//2,21)
-        for btn in self.action_btn:
-            btn.draw(self.screen)
-        #self.action_btn.draw(self.screen)
+        self.draw_text("已選動作組",900,120,isCenter=False)
+        self.draw_enable_action_info()
+        self.draw_action_button()
         pass
-
+    def draw_action_button(self):
+        for i, btn in enumerate(self.action_btn):
+            if Game1Action(i) in self.enabled_action:
+                btn.color = (0,200,0)
+            else:
+                btn.color = config.BLUE
+            btn.draw(self.screen)
+        self.all_action_select_btn.draw(self.screen)
+        self.all_action_clear_btn.draw(self.screen)
+    def draw_enable_action_info(self):
+        for i in range(len(self.enabled_action)):
+            col = i // 8
+            row = i % 8
+            self.draw_text(f"{i+1}. {self.action_name[self.enabled_action[i].value]}",1100+col*350,178+row*52,isCenter=False)
+        
     def draw_text(self, text, x, y, color=(255,255,255), isCenter = True):
         surf = self.font.render(text, True, color)
         if isCenter:
@@ -55,5 +81,19 @@ class Game1Setting:
         else:
             self.screen.blit(surf, (x, y))
 class Game1Action(Enum):
-    HH = auto()
+    HH = 0
     VV = auto()
+    VH = auto()
+    HV = auto()
+    CCWCW = auto()
+    CCWCCW = auto()
+    CWCCW = auto()
+    CWCW = auto()
+    VCW = auto()
+    VCCW = auto()
+    CCWV = auto()
+    CWV = auto()
+    HCCW = auto()
+    CWH = auto()
+    HCW = auto()
+    CCWH = auto()
